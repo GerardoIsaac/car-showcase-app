f18dece84e5b76457d9db144cf14cc5a64fd532e
Gerardo Castaneda <zl1fmercury@gmail.com>
2023-10-31 23:51:55 -0600

    add car cards with name and price

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
 
 export default async function Home() {
-  const allCars = [{}, {}, {}]//await fetchCars();
+  const allCars = await fetchCars();
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,8 +3,9 @@
 import { useState } from "react";
 import Image from "next/image";
 
-import { CarProps } from "@/types";
 import { CustomButton } from "./";
+import { CarProps } from "@/types";
+import { calculateCarRent } from "@/utils";
 
 interface CarCardProps {
   car: CarProps;
@@ -13,13 +14,19 @@ interface CarCardProps {
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
 
+  const carRent = calculateCarRent(city_mpg, year)
+
   return (
     <div className="car-card group">
       <div className="car-card__content">
         <h2 className="car-card__content-title">{make} {model}</h2>
       </div>
 
-      <p><span>Car Rent...</span></p>
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold">$</span>
+        {carRent}
+        <span className="self-end text-[14px] font-medium">/day</span>
+        </p>
     </div>
   );
 };
