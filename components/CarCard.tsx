712dc039edb843e737dcc5ac452f9e987fdbfd85
Gerardo Castaneda <zl1fmercury@gmail.com>
2023-10-31 23:37:10 -0600

    add function to calculate car rent

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client";
 
-const CarCard = () => {
-  return (
-    <div>CarCard</div>
-  )
+import { useState } from "react";
+import Image from "next/image";
+
+import { CarProps } from "@/types";
+import { CustomButton } from "./";
+
+interface CarCardProps {
+  car: CarProps;
 }
 
-export default CarCard
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">{make} {model}</h2>
+      </div>
+
+      <p><span>Car Rent...</span></p>
+    </div>
+  );
+};
+
+export default CarCard;
