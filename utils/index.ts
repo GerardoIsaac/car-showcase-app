aa746c57a1060d5c5a7e99ee5b2ac866923afeff
Gerardo Castaneda <zl1fmercury@gmail.com>
2023-11-07 14:57:30 -0600

    add env keys

--- utils/index.ts
+++ utils/index.ts
@@ -3,8 +3,8 @@ import { CarProps, FilterProps } from "@/types";
 export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
 
-  const headers = {
-    "X-RapidAPI-Key": "7704fbcb3bmshc8286741c7c9ef0p1adfdcjsna325052230ca",
+  const headers: HeadersInit = {
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || '',
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
@@ -40,7 +40,7 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
 
   const { make, year, model } = car;
 
-  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("customer", process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
   url.searchParams.append("make", make);
   url.searchParams.append("modelFamily", model.split(" ")[0]);
   url.searchParams.append("zoomType", "fullscreen");
